e9b228088728784daecd9219238175d508c17c99
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-03-10 18:38:04 -0300

    feat: add auth guard to clientes route for enhanced security

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   {
     path: 'clientes',
     loadComponent: () =>
       loadRemoteModule('clientes', './Component').then((m) => m.AppComponent),
+    canActivate: [authGuard],
   },
 ];
