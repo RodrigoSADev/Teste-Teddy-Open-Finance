0d0d65f037bdabf7792f5d0392f534915c9d205f
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-03-10 15:01:08 -0300

    refactor: remove HeaderComponent from app component and template

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-shell-root',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
