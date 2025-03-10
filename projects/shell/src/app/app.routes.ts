e6e0efce73ed67d5ec780b275e53981a64e9f38f
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-03-10 14:56:38 -0300

    feat: update routing to include login component and lazy load clientes module

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+  {
+    path: 'clientes',
+    loadComponent: () =>
+      loadRemoteModule('clientes', './Component').then((m) => m.AppComponent),
+  },
+];
