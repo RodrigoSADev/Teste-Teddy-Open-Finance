42ff8566e72dba4df0dadd6d956a614768a24f18
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-03-10 15:00:54 -0300

    feat: add HeaderComponent to clientes app component

--- projects/clientes/src/app/app.component.ts
+++ projects/clientes/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../../shell/src/app/components/header/header.component';
 
 @Component({
   selector: 'app-clientes-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'clientes';
