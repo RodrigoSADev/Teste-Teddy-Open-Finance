282e37118a7ab44749f31fddc3f64193e541d471
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-03-10 18:38:26 -0300

    feat: enhance login component with improved UI and integrate AuthService for user login

--- projects/shell/src/app/components/login/login.component.ts
+++ projects/shell/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-shell-login',
@@ -11,13 +12,16 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   formBuilder = inject(FormBuilder);
   router = inject(Router);
+  authService = inject(AuthService);
 
   loginForm = this.formBuilder.group({
-    user: ['', [Validators.required]],
+    username: ['', [Validators.required]],
   });
 
-  onSubmit() {
+  onLogin() {
     if (this.loginForm.valid) {
+      const username = this.loginForm.get('username')?.value;
+      this.authService.login(username || '');
       this.router.navigate(['/clientes']);
     }
   }
