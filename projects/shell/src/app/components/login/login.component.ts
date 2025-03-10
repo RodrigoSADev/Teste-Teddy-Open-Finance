bc810d111ac2fcb76fdf18b63f408f286d052539
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-03-10 15:06:20 -0300

    feat: enhance login component with reactive form and navigation

--- projects/shell/src/app/components/login/login.component.ts
+++ projects/shell/src/app/components/login/login.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shell-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  formBuilder = inject(FormBuilder);
+  router = inject(Router);
 
+  loginForm = this.formBuilder.group({
+    user: ['', [Validators.required]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.router.navigate(['/clientes']);
+    }
+  }
 }
