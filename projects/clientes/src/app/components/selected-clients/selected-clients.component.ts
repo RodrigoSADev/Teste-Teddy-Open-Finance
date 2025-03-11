d700b7d5841fb24c775b7e1473b7a3abbc3cb2ba
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-03-11 14:32:02 -0300

    feat: add selected clients component with template, styles, and tests

---
+++ projects/clientes/src/app/components/selected-clients/selected-clients.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes-selected-clients',
+  imports: [],
+  templateUrl: './selected-clients.component.html',
+  styleUrl: './selected-clients.component.scss'
+})
+export class SelectedClientsComponent {
+
+}
