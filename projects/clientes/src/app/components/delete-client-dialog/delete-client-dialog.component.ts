9f6bb6c32ba6d266392af3079deb248fe15c60f2
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-03-11 14:05:46 -0300

    feat: update delete client dialog for improved confirmation flow

--- projects/clientes/src/app/components/delete-client-dialog/delete-client-dialog.component.ts
+++ projects/clientes/src/app/components/delete-client-dialog/delete-client-dialog.component.ts
@@ -1,10 +1,14 @@
 import { Component, inject, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogClose,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { Client } from '../../interfaces/client.interface';
 
 @Component({
   selector: 'app-clientes-delete-client-dialog',
-  imports: [],
+  imports: [MatDialogClose],
   templateUrl: './delete-client-dialog.component.html',
   styleUrl: './delete-client-dialog.component.scss',
 })
@@ -12,10 +16,6 @@ export class DeleteClientDialogComponent {
   dialogRef = inject(MatDialogRef);
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { client: Client }) {}
-
-  onCancel(): void {
-    this.dialogRef.close(false);
-  }
 
   onConfirm(): void {
     this.dialogRef.close(true);
