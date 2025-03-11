7479ccc9380d1319c8fedbb469c33fb9c1f2b119
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-03-11 07:53:06 -0300

    feat: implement ClientsService for client data management with HTTP methods

---
+++ projects/clientes/src/app/services/clients.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client, ClientResponse } from '../interfaces/client.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClientsService {
+  http = inject(HttpClient);
+
+  private readonly url = 'https://boasorte.teddybackoffice.com.br/users';
+
+  getClients(page: number, limit: number): Observable<ClientResponse> {
+    return this.http.get<ClientResponse>(
+      `${this.url}?page=${page}&limit=${limit}`
+    );
+  }
+
+  getClientById(id: number): Observable<ClientResponse> {
+    return this.http.get<ClientResponse>(`${this.url}/${id}`);
+  }
+
+  createClient(client: Client): Observable<ClientResponse> {
+    return this.http.post<ClientResponse>(this.url, client);
+  }
+}
