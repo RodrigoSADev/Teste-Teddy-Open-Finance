d067ccee19303a1ae8d0450923ef774074c607ff
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-03-11 11:33:27 -0300

    feat: add button to create new client and integrate dialog for client creation

--- projects/clientes/src/app/components/list-clients/list-clients.component.ts
+++ projects/clientes/src/app/components/list-clients/list-clients.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Client } from '../../interfaces/client.interface';
 import { ClientsService } from '../../services/clients.service';
+import { CreateClientDialogComponent } from '../create-client-dialog/create-client-dialog.component';
 
 @Component({
   selector: 'app-clientes-list-clients',
@@ -10,18 +12,34 @@ import { ClientsService } from '../../services/clients.service';
 })
 export class ListClientsComponent implements OnInit {
   clientsService = inject(ClientsService);
+  dialog = inject(MatDialog);
 
   clients = signal<Client[]>([]);
 
   ngOnInit(): void {
+    this.loadClients();
+  }
+
+  loadClients(): void {
     this.clientsService.getClients(1, 10).subscribe({
       next: (response) => {
         this.clients.set(response.clients);
-        console.log(response);
       },
       error: (err) => {
         console.error(err);
       },
+    });
+  }
+
+  onCreateClient(): void {
+    const dialogRef = this.dialog.open(CreateClientDialogComponent, {
+      width: '500px',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.loadClients();
+      }
     });
   }
 }
